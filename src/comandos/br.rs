19b00ffb5124a94a566dc0a1afd2f4b425e3fd9c
Denis Radu <mariodenisradu1705@gmail.com>
2024-10-17 19:33:57 +0200

    comandos mover y borrar

--- src/comandos/br.rs
+++ src/comandos/br.rs
@@ -1,1 +1,8 @@
-pub fn cmd_br(destino: &str) {}
+use std::fs;
+
+pub fn cmd_br(destino: &str) {
+    match fs::remove_file(destino) {
+        Ok(_) => println!("El archivo {} ha sido eliminado", destino),
+        Err(_) => eprintln!("Error al eliminar {}", destino),
+    }
+}
--- src/comandos/mv.rs
+++ src/comandos/mv.rs
@@ -1,1 +1,8 @@
-pub fn cmd_mv(origen: &str, destino: &str) {}
+use std::fs;
+
+pub fn cmd_mv(origen: &str, destino: &str) {
+    match fs::rename(origen, destino) {
+        Ok(_) => println!("Archivo movido de {} a {}", origen, destino),
+        Err(_) => eprintln!("Error al mover el archivo de {} a {}", origen, destino),
+    }
+}
